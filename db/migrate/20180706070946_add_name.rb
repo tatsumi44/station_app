d73e35feb03e188e35bc5c7d0d66772ea0b15947
vagrant <vagrant@localhost.localdomain>
2018-07-06 17:19:16 +0900

    sample3

--- db/migrate/20180706070946_add_name.rb
+++ db/migrate/20180706070946_add_name.rb
@@ -1,5 +1,5 @@
 class AddName < ActiveRecord::Migration[5.2]
   def change
-    add_column :histories, :heroku run rake db:migrate, :string, default: "apple"
+    add_column :histories, :station_name, :string, default: "apple"
   end
 end
