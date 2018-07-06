53e47ed593abc8a6d2a04a9322e9cc4c4836055c
vagrant <vagrant@localhost.localdomain>
2018-07-07 02:17:56 +0900

    add update function

--- main.rb
+++ main.rb
@@ -6,10 +6,12 @@ require 'net/http'
 require 'json'
 require 'sinatra/activerecord'
 require './models'
+enable :sessions
 
 get '/' do
   @histories = History.all.limit(10).order("created_at desc")
   @favorites = History.where(favorite1: true)
+  p session[:contents_id]
   p @histories
   erb :form
 end
@@ -56,4 +58,33 @@ post '/:id/update' do
   history.favorite1 = !history.favorite1
   history.save
   redirect '/'
+end
+
+post '/:id/update_contents' do
+  @id = params[:id]
+  p params[:id]
+  session[:contents_id] = params[:id]
+  p session[:contents_id]
+  erb :update
+end
+post '/:id/update_result' do
+  new_x = params[:new_x]
+  new_y = params[:new_y]
+  p session[:contents_id]
+  uri = URI('http://express.heartrails.com/api/json')
+  uri.query = URI.encode_www_form({
+                                      method: "getStations",
+                                      x: new_x,
+                                      y: new_y
+                                  })
+  res = Net::HTTP.get_response(uri)
+  json = JSON.parse(res.body)
+  @station = json["response"]["station"]
+  @id = params[:id]
+  history = History.find(@id)
+  history.x = params[:new_x]
+  history.y = params[:new_y]
+  history.station_name = @station[0]["name"]
+  history.save
+  erb :main
 end
